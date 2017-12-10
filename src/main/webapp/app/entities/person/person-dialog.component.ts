058b095d088cbaad14a188678e4ee895e66587a1
Guilherme Fonseca <guilherme.fonseca.eu@gmail.com>
2017-12-10 00:01:33 -0200

    Financial Move consertado e traduções quebradas

--- src/main/webapp/app/entities/person/person-dialog.component.ts
+++ src/main/webapp/app/entities/person/person-dialog.component.ts
@@ -18,6 +18,7 @@ export class PersonDialogComponent implements OnInit {
 
     person: Person;
     isSaving: boolean;
+    hasError: boolean;
 
     constructor(
         public activeModal: NgbActiveModal,
@@ -29,6 +30,7 @@ export class PersonDialogComponent implements OnInit {
 
     ngOnInit() {
         this.isSaving = false;
+        this.hasError = false;
     }
 
     clear() {
@@ -37,6 +39,7 @@ export class PersonDialogComponent implements OnInit {
 
     save() {
         this.isSaving = true;
+
         if (this.person.id !== undefined) {
             this.subscribeToSaveResponse(
                 this.personService.update(this.person));
@@ -63,6 +66,87 @@ export class PersonDialogComponent implements OnInit {
 
     private onError(error: any) {
         this.alertService.error(error.message, null, null);
+    }
+
+    public validateCPF(): boolean {
+
+        if (this.person.cpf == null) {
+            this.hasError = true;
+            console.log('nulo');
+            return false;
+        }
+
+        const cleanCPF = this.person.cpf.replace('.', '').replace('-', '');
+
+        if (cleanCPF.length !== 11) {
+            this.hasError = true;
+            console.log('tama');
+            return false;
+        }
+        if ((cleanCPF === '00000000000') ||
+            (cleanCPF === '11111111111') ||
+            (cleanCPF === '22222222222') ||
+            (cleanCPF === '33333333333') ||
+            (cleanCPF === '44444444444') ||
+            (cleanCPF === '55555555555') ||
+            (cleanCPF === '66666666666') ||
+            (cleanCPF === '77777777777') ||
+            (cleanCPF === '88888888888') ||
+            (cleanCPF === '99999999999')) {
+            this.hasError = true;
+            console.log('fake');
+            return false;
+        }
+        let numero = 0;
+        let caracter = '';
+        const numeros = '0123456789';
+        let j = 10;
+        let somatorio = 0;
+        let resto = 0;
+        let digito1 = 0;
+        let digito2 = 0;
+        let cpfAux = '';
+        cpfAux = cleanCPF.substring(0, 9);
+        for (let i = 0; i < 9; i++) {
+            caracter = cpfAux.charAt(i);
+            if (numeros.search(caracter) === -1) {
+                this.hasError = true;
+                console.log('-1');
+                return false;
+            }
+            numero = Number(caracter);
+            somatorio = somatorio + (numero * j);
+            j--;
+        }
+        resto = somatorio % 11;
+        digito1 = 11 - resto;
+        if (digito1 > 9) {
+            digito1 = 0;
+        }
+        j = 11;
+        somatorio = 0;
+        cpfAux = cpfAux + digito1;
+        for (let h = 0; h < 10; h++) {
+            caracter = cpfAux.charAt(h);
+            numero = Number(caracter);
+            somatorio = somatorio + (numero * j);
+            j--;
+        }
+        resto = somatorio % 11;
+        digito2 = 11 - resto;
+        if (digito2 > 9) {
+            digito2 = 0;
+        }
+        cpfAux = cpfAux + digito2;
+        if (cleanCPF !== cpfAux) {
+            this.hasError = true;
+            console.log('diff');
+            return false;
+        }
+
+        this.hasError = false;
+        console.log('igu');
+        return true;
     }
 }
 
